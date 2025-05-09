238791587d8da6d0c937615e278a475578d73baf
Tetiana <yatan.shan94@gmail.com>
2025-05-08 19:34:12 -0700

    Solution

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def copy_file(comm: str) -> None:
         return
 
     try:
-        with open(src, "r") as file_in, open(dst, "w") as file_out:
+        with open(src, "rb") as file_in, open(dst, "wb") as file_out:
             file_out.write(file_in.read())
     except FileNotFoundError:
         pass
